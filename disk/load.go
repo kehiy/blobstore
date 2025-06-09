0a75fe395a93a6b2c364ff568b18190d2ea87e01
Kay <kehiiiiya@gmail.com>
2025-06-09 07:00:50 +0000

    fix(disk): not found error

--- disk/load.go
+++ disk/load.go
@@ -2,11 +2,19 @@ package disk
 
 import (
 	"context"
+	"errors"
 	"io"
 	"os"
 	"path"
 )
 
 func (d Disk) Load(_ context.Context, sha256 string) (io.ReadSeeker, error) {
-	return os.Open(path.Join(d.Path, sha256))
+	f, err := os.Open(path.Join(d.Path, sha256))
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return f, nil
 }
